a6f7864c573e7ce0be4e7226d8d6197320db2174
Dmitrii Korotov <gugameiru@gmail.com>
2020-07-23 13:54:28 +0300

    21 lesson hard is started

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
